e9c817c74ca92f79f51e877673541549789bbbe6
bio8oid <8bollod8@mail.co.uk>
2019-04-10 22:27:24 +0200

    Add centering map and markers loop in js file

--- js/script.js
+++ js/script.js
@@ -6,15 +6,87 @@
 
 
 // Initialize and add the map
+
 window.initMap = function(){
-  // The location of Uluru
-  var uluru = {lat: 35.680, lng: 139.770};
-  // The map, centered at Uluru
+  // The location 
+
+  var location = [
+
+  {
+  	lat: 35.680,
+  	lng: 139.770
+  },
+  {
+  	lat: 48.856,
+  	lng: 2.351
+  },
+  {
+  	lat: 50.850,
+  	lng: 4.360
+  },
+  {
+  	lat: 47.560,
+  	lng: 7.590
+  },
+  {
+  	lat: 13.790,
+  	lng: 100.510
+  }
+  ];
+  
+  // The map, centered at location
+
+ var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 3, center: location[0]});
+
+// Loop placing markers on map
+
+  for(var i = 0; i < location.length; i++){
+	var marker = new google.maps.Marker({position: location[i], map: map});
+	console.log(marker);
+	}
+
+	
+
+/*
+  var infos = document.querySelectorAll('#infos');
+console.log(infos);
+
+  marker.addListener('click', function(){
+
+			for(var i = 0; i < infos.length; i++){
+		infos[i].innerHTML = 'You clicked marker toko';
+	}
+});	
+
+ */
+
+
+
+/*
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 8, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+      document.getElementById('map'), {zoom: 8, center: paris});
+
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 8, center: brussels});
+
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 8, center: basel});
+
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 8, center: bangkok});
+
+  // The marker, positioned at location
+  var markerTokyo = new google.maps.Marker({position: tokyo, map: map});
+  var markerParis = new google.maps.Marker({position: paris, map: map});
+  var markerBrussels = new google.maps.Marker({position: brussels, map: map});
+  var markerBasel = new google.maps.Marker({position: basel, map: map});
+  var markerBangkok = new google.maps.Marker({position: bangkok, map: map});
+*/
+
+
 };
+
 
 
 // Mustache \\
@@ -47,8 +119,13 @@ var flkty = new Flickity( elem, {
   cellAlign: 'left',
   pageDots: false,
   hash: true,
+  //autoPlay: true
 });
 
+flkty.on( 'select', function( index ) {
+
+
+});
 
 // Progress Bar \\
 
