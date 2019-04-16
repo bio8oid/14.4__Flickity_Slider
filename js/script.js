05ea9b334d98b4e16a8eaf7e96ae13a0ffa483e8
bio8oid <47393762+bio8oid@users.noreply.github.com>
2019-04-16 17:26:20 +0200

    Fix Mustache render issue (#1)

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ var map = new google.maps.Map(document.getElementById('map'), {zoom: 3, center: 
 var marker = new google.maps.Marker({position: carouselData[0].coords, map: map});
 
 // Loop placing markers on map
+
 for(let i in carouselData){
 	marker[i] = new google.maps.Marker({position: carouselData[i].coords, map: map});
 
@@ -22,11 +23,13 @@ for(let i in carouselData){
         flkty.select(i);
       });
 }
+  
 
 // Pan and zoom acording to carousel cell number
 
 var flkty = new Flickity('.carousel');
 flkty.on( 'change', function( index ) {
+
 	map.setZoom(3);
 	map.panTo(carouselData[index].coords);
 	map.setZoom(7);
@@ -59,6 +62,10 @@ var flkty = new Flickity( elem, {
   //autoPlay: true
 });
 
+flkty.on( 'select', function( index ) {
+
+
+});
 
 // Progress Bar \\
 
